ebe6e9c1ccf1a7ae7c35eebfe8c6dd3777b5576a
Du Pham <phamxuandu13102003@gmail.com>
2023-12-21 15:33:09 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    disableStaticImages: true,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
 
 module.exports = nextConfig
